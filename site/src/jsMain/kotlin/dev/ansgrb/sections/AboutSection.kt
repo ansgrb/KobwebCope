a0e5ea34768f4e71aa3fe79eca65ec37373ab868
ansgrb <ansgrb@gmail.com>
2025-04-27 18:33:28 +0300

    Add AboutSection UI and enhance font support.
    Implemented the AboutSection layout with styling and content to display "about me" details, including a description list and a tagline. Updated font assets and applied Noto Sans with italic and bold styles in SectionsWrapper for consistent typography.

--- site/src/jsMain/kotlin/dev/ansgrb/sections/AboutSection.kt
+++ site/src/jsMain/kotlin/dev/ansgrb/sections/AboutSection.kt
@@ -1,6 +1,94 @@
 package dev.ansgrb.sections
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.CSSLengthOrPercentageNumericValue
+import com.varabyte.kobweb.compose.css.FontStyle
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontStyle
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.modifiers.gap
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.textAlign
+import com.varabyte.kobweb.silk.components.text.SpanText
+import dev.ansgrb.models.Theme
+import org.jetbrains.compose.web.css.keywords.auto
+import org.jetbrains.compose.web.css.px
 
 @Composable
-fun AboutSection() {}
+fun AboutSection() {
+	Box(
+		Modifier
+			.fillMaxWidth()
+			.padding(topBottom = 80.px, leftRight = 16.px)
+			.backgroundColor(Theme.SECTION_BG.rgb)
+	) {
+		Column(
+			horizontalAlignment = Alignment.CenterHorizontally,
+			modifier = Modifier
+				.fillMaxWidth()
+				.maxWidth(896.px)
+				.margin(leftRight = auto as CSSLengthOrPercentageNumericValue)
+		) {
+			SpanText(
+				text = "about me",
+				modifier = Modifier
+					.color(Theme.TEXT_PRIMARY.rgb)
+					.fontSize(36.px)
+					.fontWeight(FontWeight.SemiBold)
+					.margin(bottom = 24.px)
+					.textAlign(TextAlign.Center)
+			)
+			Column(
+				horizontalAlignment = Alignment.Start,
+				modifier = Modifier
+					.align(Alignment.Start)
+					.gap(8.px)
+			) {
+				val listItems = listOf(
+					"kotlin multiplatform (mobile, desktop, backend)",
+					"blockchain tinkering (solidity + web3)",
+					"chaos engineering & robust pipelines",
+					"golang services & micro-backend"
+				)
+				listItems.forEach { item ->
+					Row(verticalAlignment = Alignment.CenterVertically) {
+						SpanText(
+							"• ",
+							Modifier
+								.color(Theme.TEXT_SECONDARY.rgb)
+								.fontSize(18.px)
+								.margin(right = 8.px)
+						)
+						SpanText(
+							item,
+							Modifier
+								.color(Theme.TEXT_SECONDARY.rgb)
+								.fontSize(18.px)
+						)
+					}
+				}
+			}
+			SpanText(
+				text = "\"compiling kmp apps | battling blockchain demons\"",
+				modifier = Modifier
+					.color(Theme.TEXT_SECONDARY.rgb)
+					.margin(top = 24.px)
+					.fontStyle(FontStyle.Italic)
+					.textAlign(TextAlign.Center)
+					.fillMaxWidth()
+			)
+		}
+	}
+}
--- site/src/jsMain/kotlin/dev/ansgrb/sections/SectionsWrapper.kt
+++ site/src/jsMain/kotlin/dev/ansgrb/sections/SectionsWrapper.kt
@@ -1,10 +1,13 @@
 package dev.ansgrb.sections
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.FontStyle
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontStyle
 
 @Composable
 fun SectionsWrapper() {
@@ -12,6 +15,8 @@ fun SectionsWrapper() {
 		horizontalAlignment = Alignment.CenterHorizontally,
 		modifier = Modifier
 			.fillMaxSize()
+			.fontFamily("Noto Sans")
+			.fontStyle(FontStyle.Italic)
 	) {
 		HeroSection()
 		AboutSection()
